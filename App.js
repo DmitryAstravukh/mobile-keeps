61efa3c7955b76df7abfebecc6c2a2a45307272d
MrFreeman <astrauh1.dima1@ya.ru>
2020-09-06 15:39:06 +0300

    add navigation, screens components

--- App.js
+++ App.js
@@ -1,82 +1,19 @@
 import React from 'react';
-import { Text, SectionList } from 'react-native';
-import styled from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Keep from './components/keep';
-import GroupTitle from './components/group-title';
-import AddButton from './components/add-button';
+import { Home } from './screens';
 
-const DATA = [
-  {
-    title: '11 сентября',
-    data: [
-      {
-        title: 'Заголовок 1',
-        text: 'Текст 1',
-        color: '#ff5750'
-      },
-      {
-        title: 'Заголовок 2',
-        text: 'Текст 2',
-        color: '#ffa4a0'
-      },
-      {
-        title: 'Заголовок 3',
-        text: 'Текст 3',
-        color: 'green'
-      }
-    ]
-  },
-  {
-    title: '12 сентября',
-    data: [
-      {
-        title: 'Заголовок 1',
-        text: 'Текст 1',
-        color: 'gray'
-      },
-      {
-        title: 'Заголовок 2',
-        text: 'Текст 2',
-        color: 'grey'
-      },
-      {
-        title: 'Заголовок 3',
-        text: 'Текст 3',
-        color: 'green'
-      }
-    ]
-  },
-];
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Container>
-      <SectionList
-        sections={DATA}
-
-        keyExtractor={
-          (item, index) => item + index
-        }
-
-        renderItem={
-          ({ item }) => <Keep {...item}/> 
-        }
-
-        renderSectionHeader={
-          ({ section: { title } }) => (
-            <GroupTitle title={title} />
-          )
-        }
-      />
-      
-      <AddButton />
-    </Container>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Заметки' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const Container = styled.View`
-  flex: 1;
-  margin-top: 25px;
-`;
 
---
+++ screens/home.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { SectionList } from 'react-native';
+import styled from 'styled-components/native';
+
+import Keep from './../components/keep';
+import GroupTitle from './../components/group-title';
+import AddButton from './../components/add-button';
+
+const DATA = [
+  {
+    title: '11 сентября',
+    data: [
+      {
+        title: 'Заголовок 1',
+        text: 'Текст 1',
+        color: '#ff5750'
+      },
+      {
+        title: 'Заголовок 2',
+        text: 'Текст 2',
+        color: '#ffa4a0'
+      },
+      {
+        title: 'Заголовок 3',
+        text: 'Текст 3',
+        color: 'green'
+      }
+    ]
+  },
+  {
+    title: '12 сентября',
+    data: [
+      {
+        title: 'Заголовок 1',
+        text: 'Текст 1',
+        color: 'gray'
+      },
+      {
+        title: 'Заголовок 2',
+        text: 'Текст 2',
+        color: 'grey'
+      },
+      {
+        title: 'Заголовок 3',
+        text: 'Текст 3',
+        color: 'green'
+      }
+    ]
+  },
+];
+
+export function Home() {
+  return (
+    <Container>
+      <SectionList
+        sections={DATA}
+
+        keyExtractor={
+          (item, index) => item + index
+        }
+
+        renderItem={
+          ({ item }) => <Keep {...item}/> 
+        }
+
+        renderSectionHeader={
+          ({ section: { title } }) => (
+            <GroupTitle title={title} />
+          )
+        }
+      />
+      
+      <AddButton />
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  flex: 1;
+`;
+
